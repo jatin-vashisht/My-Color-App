fe5865e56d16fb9d23e7ca8bffc4a1ec39b9afc3
unknown <jatinvashisht11@gmail.com>
2023-09-03 20:35:45 +0530

    Added Snackbar and styled Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Select from '@mui/material/Select/Select';
+import MenuItem from '@mui/material/MenuItem/MenuItem';
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 import Slider from 'rc-slider'
 import 'rc-slider/assets/index.css'
 import './Navbar.css'
 
-export default function Navbar({level,changeLevel}) {
+export default function Navbar({ level, changeLevel,changeFormat }) {
+    const [format, setFormat] = useState('hex')
+    const [open, setOpen] = useState(true)
+    const handleChange = (e) => {
+        setFormat(e.target.value)
+        setOpen(true)
+        changeFormat(e.target.value)
+    }
+    const closeSnackBar = () => {
+        setOpen(false)
+    }
   return (
       <header className='Navbar'>
         <div className="logo">
@@ -15,6 +30,35 @@ export default function Navbar({level,changeLevel}) {
                 <Slider defaultValue={level} min={100} max={900} step={100} onChange={changeLevel} />
             </div>      
         </div>
+        <div className="select-container">
+            <Select value={format} onChange={handleChange}>
+                <MenuItem value='hex'>HEX - #ffffff</MenuItem>        
+                <MenuItem value='rgb'>RGB - rgb(255,255,255)</MenuItem>        
+                <MenuItem value='rgba'>RGBA - rgba(255,255,255,1.0)</MenuItem>        
+            </Select>
+          </div>
+          <Snackbar
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+              open={open}
+              autoHideDuration={3000}
+              message={
+                  <span id='message-id'>Format Changed To {format.toUpperCase()}</span>
+              }
+              ContentProps={{
+                'aria-describedby': 'message-id'
+              }}
+              onClose={closeSnackBar}
+              action={[
+                  <IconButton
+                    onClick={closeSnackBar}
+                    color='inherit'
+                    key='close'
+                    aria-label='close'
+                  >
+                      <CloseIcon />
+                  </IconButton>
+              ]}
+          />
     </header>
   )
 }
--- src/Pallete.js
+++ src/Pallete.js
@@ -5,16 +5,20 @@ import Navbar from './Navbar'
 
 export default function Pallete(pallete) {
     const [level, setLevel] = useState(500)
+    const [format, setFormat] = useState('hex')
     const {colors} = pallete.pallete
     const colorBoxes = colors[level].map(color => (
-        <ColorBox background={color.hex} name={color.name} key={color.name} />
+        <ColorBox background={color[format]} name={color.name} key={color.name} />
     ))
     const changeLevel = (level) => {
         setLevel(level)
     }
+    const changeFormat = (val) => {
+        setFormat(val)
+    }
   return (
       <div className='Pallete'>
-        <Navbar level={level} changeLevel={changeLevel} />
+        <Navbar level={level} changeLevel={changeLevel} changeFormat={changeFormat} />
         <div className="Pallete-colors">
             { colorBoxes}
         </div>
