ddaeb7503dc5620d2e4e6cf392d047b1bb9effd6
unknown <jatinvashisht11@gmail.com>
2023-09-03 20:55:24 +0530

    Updated Footer

---
+++ src/Palette.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import ColorBox from './ColorBox'
+import './Palette.css'
+import Navbar from './Navbar'
+
+export default function Palette(palette) {
+    const [level, setLevel] = useState(500)
+    const [format, setFormat] = useState('hex')
+    const {colors,paletteName,emoji} = palette.palette
+    const colorBoxes = colors[level].map(color => (
+        <ColorBox background={color[format]} name={color.name} key={color.id} />
+    ))
+    const changeLevel = (level) => {
+        setLevel(level)
+    }
+    const changeFormat = (val) => {
+        setFormat(val)
+    }
+  return (
+      <div className='Palette'>
+        <Navbar level={level} changeLevel={changeLevel} changeFormat={changeFormat} />
+        <div className="Palette-colors">
+            { colorBoxes}
+          </div>
+          <footer className='Palette-footer'>
+              {paletteName}
+              <span className='emoji'>{emoji}</span>
+          </footer>
+    </div>
+  )
+}
