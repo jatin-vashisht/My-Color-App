7ba8ee16d1316c1a025a1746147329a8ca78ee5b
unknown <jatinvashisht11@gmail.com>
2023-09-05 20:42:10 +0530

    Finished ColorBox Styles

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -68,6 +68,68 @@ export default function ColorBox({ name, background, id, paletteId,showingFullPa
             textTransform: 'uppercase',
             textDecoration: 'none',
             opacity: 0
+        },
+        boxContent: {
+            position: 'absolute',
+            width: '100%',
+            left: '0',
+            bottom: '0',
+            padding: '10px',
+            color: 'black',
+            letterSpacing: '1px',
+            textTransform: 'uppercase',
+            fontSize: '12px'
+        },
+        copyOverlay: {
+            opacity: '0',
+            zIndex: '0',
+            width: '100%',
+            height: '100%',
+            transition: 'transform 0.6s ease-in-out',
+            transform: 'scale(0.1)'
+        },
+        showOverlay: {
+            opacity: '1',
+            transform: 'scale(50)',
+            zIndex: '10',
+            position: 'absolute'
+        },
+        copyMsg: {
+            position: 'fixed',
+            left: '0',
+            right: '0',
+            top: '0',
+            bottom: '0',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            fontSize: '4rem',
+            transform: 'scale(0.1)',
+            opacity: '0',
+            color: 'white',
+            'h1': {
+                fontWeight: '400',
+                textShadow: '1px 2px black',
+                background: 'rgba(255,255,255,0.3)',
+                width: '100%',
+                textAlign: 'center',
+                marginBottom: '0',
+                padding: '1rem',
+                textTransform: 'uppercase',
+                opacity: 1
+            },
+            'p': {
+                fontSize: '2rem',
+                fontWeight: '100'
+            }
+        },
+        showMsg: {
+            opacity: 1,
+            transform: 'scale(1)',
+            zIndex: '25',
+            transition: 'all 0.4s ease-in-out',
+            transitionDelay: '0.3s'
         }
     }))
     const { classes } = useStyles(lum)    // make sure to pass the changing param in useStyles too ,
@@ -75,13 +137,13 @@ export default function ColorBox({ name, background, id, paletteId,showingFullPa
     return (
         <CopyToClipboard text={background} onCopy={changeCopyState}>
             <div style={{ background }} className={classes.ColorBox}>
-                <div style={{ background }} className={`copy-overlay ${copied && 'show' }`} />  {/* this is to put an overlay of the same color*/}
-                <div className={`copy-msg ${copied && 'show' }`}>  {/* we can't put text in above div , as the div is scaling so text will also scale*/}
+                <div style={{ background }} className={`${classes.copyOverlay} ${copied && classes.showOverlay}`} />  {/* this is to put an overlay of the same color*/}
+                <div className={`${classes.copyMsg} ${copied && classes.showMsg }`}>  {/* we can't put text in above div , as the div is scaling so text will also scale*/}
                     <h1>copied!</h1>
                     <p className={classes.copyText}>{background}</p>
                 </div>
-                <div className="copy-container">
-                    <div className="box-content">
+                <div>
+                    <div className={classes.boxContent}>
                         <span className={classes.colorName}>{name}</span>
                     </div>
                     <button className={classes.copyButton}>Copy</button>
