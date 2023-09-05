8d11fa5d95d3c1262d5f734d63b979df6535831d
unknown <jatinvashisht11@gmail.com>
2023-09-05 21:12:00 +0530

    Refactor SingleColorPalette Styles

--- src/Palette.js
+++ src/Palette.js
@@ -3,7 +3,7 @@ import ColorBox from './ColorBox'
 import './Palette.css'
 import Navbar from './Navbar'
 import PaletteFooter from './PaletteFooter'
-
+import { makeStyles } from "tss-react/mui";
 
 export default function Palette(palette) {
     const [level, setLevel] = useState(500)
@@ -18,10 +18,34 @@ export default function Palette(palette) {
     const changeFormat = (val) => {
         setFormat(val)
     }
+  
+  const useStyles = makeStyles()(theme => ({
+    Palette: {
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column'
+    },
+    colors: {
+      height: '90%'
+    },
+    footer: {
+      backgroundColor: '#fff',
+      height: '5vh',
+      display: 'flex',
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      fontWeight: 'bold'
+    },
+    emoji: {
+      fontSize: '1.5rem',
+      margin: '0 1rem'
+    }
+  }))
+  const {classes} = useStyles()
   return (
-      <div className='Palette'>
+      <div className={classes.Palette}>
         <Navbar level={level} changeLevel={changeLevel} changeFormat={changeFormat} showingAllColors />
-        <div className="Palette-colors">
+        <div className={classes.colors}>
             { colorBoxes}
           </div>
         <PaletteFooter paletteName={paletteName} emoji={emoji} />
--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -3,6 +3,7 @@ import ColorBox from './ColorBox'
 import Navbar from './Navbar'
 import PaletteFooter from './PaletteFooter'
 import { Link } from 'react-router-dom'
+import { makeStyles } from "tss-react/mui";
 
 export default function SingleColorPalette({ allShades,palette }) {
   const [format, setFormat] = useState('hex')
@@ -12,14 +13,55 @@ export default function SingleColorPalette({ allShades,palette }) {
   ))
   const changeFormat = (val) => {
     setFormat(val)
-}
+  }
+  const useStyles = makeStyles()(theme => ({
+    Palette: {
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column'
+    },
+    colors: {
+      height: '90%'
+    },
+    goBack: {
+      width: '20%',
+      height: '50%',
+      margin: '0 auto',
+      display: 'inline-block',
+      position: 'relative',
+      cursor: 'pointer',
+      marginBottom: '-3.5px',
+      opacity: 1,
+      backgroundColor: 'black',
+      'a': {
+        color: 'white',
+        width: '100px',
+        height: '30px',
+        position: 'absolute',
+        display: 'inline-block',
+        top: '50%',
+        left: '50%',
+        marginLeft: '-50px',
+        marginTop: '-15px',
+        textAlign: 'center',
+        outline: 'none',
+        background: 'rgba(255,255,255,0.3)',
+        border: 'none',
+        fontSize: '1rem',
+        lineHeight: '30px',
+        textTransform: 'uppercase',
+        textDecoration: 'none',
+      }
+    }
+  }))
+  const {classes} = useStyles()
   return (
-    <div className='SingleColorPalette Palette'>
+    <div className={classes.Palette}>
       <Navbar changeFormat={changeFormat} showingFullPalette={false} />
-      <div className='Palette-colors'>
+      <div className={classes.colors}>
         {colorBoxes}
-        <div className='go-back ColorBox'>
-          <Link to={`/palette/${id}`} className="back-button">Go Back</Link>
+        <div className={classes.goBack}>
+          <Link to={`/palette/${id}`}>Go Back</Link>
         </div>
       </div>
       <PaletteFooter paletteName={paletteName} emoji={emoji} />
