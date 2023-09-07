56c3f14f89887ed5f748d57222ea6f0c1a7bc891
unknown <jatinvashisht11@gmail.com>
2023-09-07 22:14:37 +0530

    Made PaletteList responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,35 +1,47 @@
 import { makeStyles } from "tss-react/mui";
+import sizes from "./sizes";
 export default makeStyles()(theme => ({
-  root: {
-      backgroundColor: 'blue',
-      height: '100vh',
-      display: 'flex',
-      alignItems: 'flex-start',
-      justifyContent: 'center'
-  },
-  container: {
-      width: '50%',
-      display: 'flex',
-      alignItems: 'flex-start',
-      flexDirection: 'column',
-      flexWrap: 'wrap'
-  },
-  nav: {
-      display: 'flex',
-      width: '100%',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      color: "white",
-      'a': {
-          color: 'white',
-          textDecoration: 'none'
-      }
-  },
-  palettes: {
-      boxSizing: 'border-box',
-      width: '100%',
-      display: 'grid',
-      gridTemplateColumns: 'repeat(3,30%)',
-      gap: '5%'
-  }
+    root: {
+        backgroundColor: 'blue',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'flex-start',
+        justifyContent: 'center'
+    },
+    container: {
+        width: '60%',
+        display: 'flex',
+        alignItems: 'flex-start',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+        [sizes.down('lg')]: {
+            width: '70%'
+        }
+    },
+    nav: {
+        display: 'flex',
+        width: '100%',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        color: "white",
+        'a': {
+            color: 'white',
+            textDecoration: 'none'
+        }
+    },
+    palettes: {
+        boxSizing: 'border-box',
+        width: '100%',
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3,30%)',
+        gap: '2rem',
+        [sizes.down('md')]: {
+            gridTemplateColumns: 'repeat(2,50%)',
+            gap: '1.5rem',
+        },
+        [sizes.down('xs')]: {
+            gridTemplateColumns: 'repeat(1,100%)',
+            gap: '1rem',
+        }
+    }
 }))
